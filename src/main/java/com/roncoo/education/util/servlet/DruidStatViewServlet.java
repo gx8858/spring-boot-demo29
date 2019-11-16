96ffeb16bdf7f002d2d845b8735f3b7901c05c68
gx8858 <1027829779@qq.com>
2019-11-16 14:44:49 +0800

    访问http://localhost:8080/druid会有druid的管理工具

--- src/main/java/com/roncoo/education/util/servlet/DruidStatViewServlet.java
+++ src/main/java/com/roncoo/education/util/servlet/DruidStatViewServlet.java
@@ -8,6 +8,7 @@ import com.alibaba.druid.support.http.StatViewServlet;
 /**
  *
  * @author wujing
+   *  访问http://localhost:8080/druid会有druid的管理工具
  */
 @WebServlet(urlPatterns = { "/druid/*" }, initParams = { @WebInitParam(name = "loginUsername", value = "roncoo"), @WebInitParam(name = "loginPassword", value = "roncoo") })
 public class DruidStatViewServlet extends StatViewServlet {
